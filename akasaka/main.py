95ee5d64c73670ab8b850c71f54b8f5fa69aea4a
JeffersonQin <gyrojeff@foxmail.com>
2024-04-01 20:33:21 -0400

    fix: no need to try catch

--- akasaka/main.py
+++ akasaka/main.py
@@ -35,36 +35,32 @@ def main():
     if args.help:
         remaining_args += ["-h"]
 
-    try:
-        # Add the current working directory to the sys.path so that the module can be found
-        sys.path.append(os.getcwd())
-        # Load the class from the module
-        loaded_class = load_class(module_path)
-        print(f"Successfully loaded class '{loaded_class.__name__}' from module '{args.module_path}'.")
-        # Check if the loaded class is a subclass of 'AkasakaTask'
-        if not issubclass(loaded_class, AkasakaTask):
-            raise ValueError("The loaded class should be a subclass of 'AkasakaTask'.")
+    # Add the current working directory to the sys.path so that the module can be found
+    sys.path.append(os.getcwd())
+    # Load the class from the module
+    loaded_class = load_class(module_path)
+    print(f"Successfully loaded class '{loaded_class.__name__}' from module '{args.module_path}'.")
+    # Check if the loaded class is a subclass of 'AkasakaTask'
+    if not issubclass(loaded_class, AkasakaTask):
+        raise ValueError("The loaded class should be a subclass of 'AkasakaTask'.")
 
-        # Initialize the task with the remaining command line arguments
-        akasaka_task = loaded_class(args=remaining_args)
+    # Initialize the task with the remaining command line arguments
+    akasaka_task = loaded_class(args=remaining_args)
 
-        # Generate a list of tasks to be executed
-        tasks = akasaka_task.generate_tasks()
-        tasks = list(tasks)
-        print(f"Generated {len(tasks)} tasks to be executed.")
+    # Generate a list of tasks to be executed
+    tasks = akasaka_task.generate_tasks()
+    tasks = list(tasks)
+    print(f"Generated {len(tasks)} tasks to be executed.")
 
-        pool = multiprocessing.Pool(processes=num_process)
+    pool = multiprocessing.Pool(processes=num_process)
 
-        # using the chunksize for better performance
-        # this implementation is taken from the official implementation of multiprocessing.Pool.map
-        chunksize, extra = divmod(len(tasks), num_process * 4)
-        if extra:
-            chunksize += 1
+    # using the chunksize for better performance
+    # this implementation is taken from the official implementation of multiprocessing.Pool.map
+    chunksize, extra = divmod(len(tasks), num_process * 4)
+    if extra:
+        chunksize += 1
 
-        _ = list(tqdm(pool.imap_unordered(execute, tasks, chunksize=chunksize), total=len(tasks)))
+    _ = list(tqdm(pool.imap_unordered(execute, tasks, chunksize=chunksize), total=len(tasks)))
 
-        pool.close()
-        pool.join()
-
-    except (ImportError, AttributeError, ValueError) as e:
-        print(f"Error: {e}")
+    pool.close()
+    pool.join()
