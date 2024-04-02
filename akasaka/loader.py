523bee3a001ca7e19d1e2043eba194af26c9aae2
JeffersonQin <gyrojeff@foxmail.com>
2024-04-01 20:33:09 -0400

    fix: remove redundant printing

--- akasaka/loader.py
+++ akasaka/loader.py
@@ -4,8 +4,6 @@ import importlib.util
 def load_class(module_path):
     try:
         module_name, class_name = module_path.rsplit('.', 1)
-        print(module_name)
-        print(class_name)
         spec = importlib.util.find_spec(module_name)
         if spec is None:
             raise ImportError(f"Module '{module_name}' not found.")
